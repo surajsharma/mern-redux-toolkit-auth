6d070e1a98d2427b7a83f27396500e87e3084f61
suraj <surajsharma@pm.me>
2022-02-19 13:01:27 +0530

    add heroku

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
-const express  = require('express');
+const express = require("express");
 
-const colors = require('colors');
-const dotenv = require('dotenv').config();
+const path = require("path");
+
+const colors = require("colors");
+const dotenv = require("dotenv").config();
 
 const goalRoutes = require("./routes/goalRoutes");
 const userRoutes = require("./routes/userRoutes");
@@ -10,17 +12,28 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 
 const connectDB = require("./config/db");
 
-const port = process.env.PORT||5000;
+const port = process.env.PORT || 5000;
 
 connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", goalRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(errorHandler);
+//serve frontend
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+    app.get("*", (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, "../", "frontend", "build", "index.html")
+        )
+    );
+} else {
+    app.get("/", (req, res) => res.send("Please set to production"));
+}
 
-app.listen(port, ()=>console.log(`Server on port ${port}`))
+app.use(errorHandler);
+app.listen(port, () => console.log(`Server on port ${port}`));
